9eb84b1cbfdbd913d522bf8c06927fe762da979d
AHUHUHU <XUANTRI300801@gmail.com>
2023-12-16 21:21:15 +0700

    Tong va Hieu

--- Source.cpp
+++ Source.cpp
@@ -7,28 +7,22 @@ int tong(int a, int b)
 	return a+b;
 }
 
-=======
-
 int hieu(int a, int b)
 {
 	return a-b;
 }
->>>>>>> hieu
+
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	int a,b;
-<<<<<<< HEAD
 	cout<<"Nhap a la: ";cin>>a;cout<<endl;	
-=======
 	cout<<"Nhap b la: ";cin>>b;cout<<endl;
 	cout<<"Tong cua a va b la: "<<tong(a,b);
 	cout<<"Hieu cua a va b la: "<<hieu(a,b);
 
-
->>>>>>> hieu
 	system("pause");
 	return 0;
 }
